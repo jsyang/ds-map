f141709e2b39a3bb5d742e613516525eca5a6bda
jimyang <jim.yang@thisplace.com>
2015-05-12 20:48:50 +0100

    Add in the listing menu and add localstorage caching

--- assets/index.js
+++ assets/index.js
@@ -1,66 +1,83 @@
-var dsmap = {
-    activeInfoWindows : [],
+var $body;
+var $controlsHeader;
+var $mapCover;
+var $controlsResults;
+var $mapHeader;
 
-    directionsService : new google.maps.DirectionsService(),
-    currentLatLong : null,
-    activeLatLong : null
+/**
+ * @global
+ * @type {{activeInfoWindows: Array, directionsService: google.maps.DirectionsService, currentLatLong: null}}
+ */
+var dsmap = {
+    eventData: JSON.parse(localStorage.getItem('eventData')),
+
+    activeInfoWindows: [],
+
+    directionsService: new google.maps.DirectionsService(),
+    currentLatLong   : null
 };
 
-function onCSV(res) {
-    res.data.forEach(function(event){
+function onEventMarkerClick(marker, infoWindow) {
+    closeAllInfoWindows();
+    infoWindow.open(dsmap.map, marker);
+    dsmap.activeInfoWindows.push(infoWindow);
+}
+
+function eventToResultHTML(event, i) {
+    return [
+        '<div class="result" data-id="' + i + '">',
+        '<div class="title">' + event.EVENT + '</div>',
+        '<div class="time">' + event.TIME + '</div>',
+        '</div>'
+    ].join('');
+}
+
+function eventToHTML(event) {
+    return ('<div style="text-align:center"><img src="' + event.HOST + '"></div>' +
+    '<div><h2>' + event.EVENT + '</h2></div>' +
+    '<div>' + event.ABOUT + '</div>' +
+    '<div><h3>Time</h3>' + event.TIME + '</div>' +
+    '<div><h3>Location</h3>' + event.LOCATION + '</div>');
+}
+
+function onData() {
+    dsmap.eventData.forEach(function (event) {
         var infoWindow = new google.maps.InfoWindow({
-            content: (
-                '<div style="text-align:center"><img src="'+event.HOST+'"></div>' +
-                '<div><h2>'+event.EVENT+'</h2></div>'+
-                '<div>'+event.ABOUT+'</div>' +
-                '<div><h3>Time</h3>'+event.TIME+'</div>'+
-                '<div><h3>Location</h3>'+event.LOCATION+'</div>'
-            )
+            content: eventToHTML(event)
         });
 
         var latLong = new google.maps.LatLng(event.LAT, event.LONG);
 
-        var icon = event.THISPLACE? 'assets/marker-thisplace.png' : 'assets/marker.png';
+        var icon = event.THISPLACE ? 'assets/marker-thisplace.png' : 'assets/marker.png';
 
         var marker = new google.maps.Marker({
             position: latLong,
-            map: dsmap.map,
-            icon: icon
+            map     : dsmap.map,
+            icon    : icon
         });
 
-        google.maps.event.addListener(marker, 'click', function() {
-          closeAllInfoWindows();
-          infoWindow.open(dsmap.map, marker);
-          dsmap.activeInfoWindows.push(infoWindow);
-          dsmap.activeLatLong = latLong;
-
-          showWalkingDirections();
-        });
+        google.maps.event.addListener(marker, 'click', onEventMarkerClick.bind(null, marker, infoWindow));
     });
+
+    var filteredEventsHTML = '';
+
+    dsmap.eventData.forEach(function showResult(result) {
+        filteredEventsHTML += eventToResultHTML(result);
+    });
+
+    $controlsResults.html(filteredEventsHTML);
 
     addCurrentLocationMarker();
 }
 
-function showWalkingDirections() {
-    var start = dsmap.currentLatLong;
-    var end = dsmap.activeLatLong;
-    if(start && end) {
-        dsmap.directions.setMap(dsmap.map);
-      var request = {
-          origin: start,
-          destination:end,
-          travelMode: google.maps.TravelMode.WALKING
-      };
-      dsmap.directionsService.route(request, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          dsmap.directions.setDirections(response);
-        }
-      });
-    }
+function onCSV(res) {
+    dsmap.eventData = res.data;
+    localStorage.setItem('eventData', JSON.stringify(res.data));
+    onData();
 }
 
 function closeAllInfoWindows() {
-    dsmap.activeInfoWindows.forEach(function(iw){
+    dsmap.activeInfoWindows.forEach(function (iw) {
         iw.close();
     });
 
@@ -69,10 +86,11 @@ function closeAllInfoWindows() {
     dsmap.directions.setMap(null);
 }
 
-function addCurrentLocationMarker(){
+function addCurrentLocationMarker() {
     if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var latLong = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
             var marker = new google.maps.Marker({
                 position: latLong,
                 map: dsmap.map
@@ -83,31 +101,28 @@ function addCurrentLocationMarker(){
     }
 }
 
-google.maps.event.addDomListener(window, 'load', function() {
-    // https://developers.google.com/maps/documentation/javascript/3.exp/reference
+function showDetails() {
+
+}
+
+google.maps.event.addDomListener(window, 'load', function () {
+    $body = $('body');
+    $mapHeader = $('#main .header');
+    $controlsResults = $('#controls #results');
+    $controlsHeader = $('#controls .header');
+    $mapCover = $('#map-cover');
 
     var mapOptions = {
-      zoom: 13,
-      center: new google.maps.LatLng(51.5250255,-0.0904861),
+        zoom  : 14,
+        center: new google.maps.LatLng(51.5267625, -0.0801057),
 
-      panControl: false,
-      streetViewControl: false,
+        panControl       : false,
+        streetViewControl: false,
+        mapTypeControl   : false,
 
-      mapTypeControl: true,
-    mapTypeControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_TOP,
-      style: google.maps.MapTypeControlStyle.DEFAULT,
-      mapTypeIds: [
-        google.maps.MapTypeId.ROADMAP,
-        google.maps.MapTypeId.TERRAIN
-      ]
-    },
-
-    zoomControl: true,
-    zoomControlOptions: {
-        position: google.maps.ControlPosition.TOP_RIGHT
-    }
-  };
+        zoomControl       : true,
+        zoomControlOptions: { position: google.maps.ControlPosition.TOP_RIGHT }
+    };
 
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     google.maps.event.addListener(map, 'click', closeAllInfoWindows);
@@ -118,10 +133,21 @@ google.maps.event.addDomListener(window, 'load', function() {
     dsmap.directions = directions;
     dsmap.map = map;
 
-    Papa.parse('assets/index.csv', {
-        header: true,
-        download: true,
-        dynamicTyping: true,
-    	complete: onCSV
+    if (dsmap.eventData) {
+        onData();
+    } else {
+        Papa.parse('assets/index.csv', {
+            header       : true,
+            download     : true,
+            dynamicTyping: true,
+            complete     : onCSV
+        });
+    }
+
+    $mapHeader.on('click', $body.addClass.bind($body, 'show-controls'));
+    $controlsHeader.on('click', $body.removeClass.bind($body, 'show-controls'));
+
+    $controlsResults.on('click', '.result', function () {
+        showDetails(this.getAttribute('data-id'));
     });
 });
